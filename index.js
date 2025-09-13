4ebdff309b6b34c5a284cdd9ee17604ca23d6a1f
el-sam <samurai.h@yahoo.com>
2025-09-13 22:52:45 +0800

    passed first 2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+const arr = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+let arrNew = [];
+const returnFirstTwoDrivers = (arr) => {
+     return (arrNew = [arr[0],arr[1]]);
+    // return arrNew;
+
+}
+
+const returnLastTwoDrivers = function (arr) {
+    return [arr[2],arr[3]];
+}
