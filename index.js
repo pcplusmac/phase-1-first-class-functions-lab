a3c9ff62128990437248ee9d02ff1debae84f753
el-sam <samurai.h@yahoo.com>
2025-09-14 08:55:37 +0800

    fare multiple function test passed

--- index.js
+++ index.js
@@ -10,3 +10,28 @@ const returnLastTwoDrivers = function (arr) {
     arrNew = [arr[arr.length-2],arr[arr.length-1]]
     return arrNew;
 }
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers] ;
+
+function createFareMultiplier(int) {
+    // way 1: 
+    const fn4 = function (fare) {
+        return fare*int;
+
+    }
+    return fn4;
+
+    // way 2: 
+    // return (function (fare) {return fare*int;});
+
+    // way 3: 
+    // return ( fare => {return fare*int;})
+
+    // way 4:
+    // const fn4 = fare => fare*int;
+    // return fn4;
+
+    // way 5: 
+    // return (fare => fare*int);
+}
+ 
